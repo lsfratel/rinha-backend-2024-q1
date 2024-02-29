1c86af0ee4c80b165e80984fa062f82cd9e653ab
Lucas Santana <lsfratel@gmail.com>
2024-02-28 21:09:17 -0300

    refactor: code cleanup

--- rinha/settings.py
+++ rinha/settings.py
@@ -1,5 +1,3 @@
 import os
 
-PORT = int(os.environ.get("PORT", "8080"))
-HOST = os.environ.get("HOST", "0.0.0.0")
 DATABASE_URL = os.environ.get("DATABASE_URL", "postgresql://postgres:postgres@localhost/rinha-2024-q1")
